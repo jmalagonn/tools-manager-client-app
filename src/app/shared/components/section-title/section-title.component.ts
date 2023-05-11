4fc4fcbc7ac018671583f784d5519afebf07c184
JuanNicolasMalagon <nicolas0915@hotmail.com>
2023-05-11 07:56:49 -0500

    Minor improvements

--- src/app/shared/components/section-title/section-title.component.ts
+++ src/app/shared/components/section-title/section-title.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-section-title',
@@ -8,4 +8,6 @@ import { Component, Input } from '@angular/core';
 export class SectionTitleComponent {
   @Input() title?: string;
   @Input() editableSection?: boolean;
+
+  @Output() editButtonClickedEvent = new EventEmitter<void>();
 }
